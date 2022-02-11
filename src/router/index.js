ea21a6503c7781a76dfa18002cb4b6aecafc9f08
yoku-094 <yoku.20201001@gmail.com>
2022-02-11 19:34:57 +0900

    初期設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/my-website",
     name: "Home",
     component: Home,
   },
   {
-    path: "/about",
+    path: "/my-website/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
 ];
 
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  outputDir: "docs",
+  assetsDir: "./",
+  publicPath: "./",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "my-website",
+    },
+  },
+};
